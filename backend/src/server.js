14170efce497fa01c16415268457b4e8d5a26503
Ryan <ryan@mytrellis.com>
2018-07-31 11:10:36 -0400

    Remove firebase from project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,38 +1,47 @@
 const express = require('express');
-const admin = require('firebase-admin');
 
-// Initialize connection to firebase
-admin.initializeApp({
-  credential: {
-    getAccessToken: () => ({
-      expires_in: 0,
-      access_token: '',
-    }),
-  },
-  databaseURL: 'ws://localhost:9100',
-});
-
-const db = admin.database();
+// In-memory 'database' object
+const db = {
+  sensors: [
+    {
+      id: 1,
+      name: 'North Sensor',
+      description: 'The sensor in the north'
+    },
+    {
+      id: 2,
+      name: 'South Sensor',
+      description: 'The south field sensor'
+    },
+    {
+      id: 3,
+      name: 'East Sensor',
+      description: 'The sensor on the east side'
+    },
+    {
+      id: 4,
+      name: 'West Sensor',
+      description: 'The western most sensor'
+    }
+  ]
+};
 
 // Create express app
 const app = express();
 
 app.use(function(req, res, next) {
   // Allow CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
-
 app.get('/sensors', (req, res) => {
   // Return all sensors
-  db.ref('sensors')
-    .once('value')
-    .then(snap => snap.val())
-    .then(sensors => Object.values(sensors))
-    .then(sensorsArr => res.json(sensorsArr))
-    .catch(err => res.status(500).send('Error getting sensors'));
+  res.json(db.sensors);
 });
 
 const PORT = 9000;
