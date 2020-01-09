490f5f2351c410f0787c46b34ab3423ffb8435f7
Ryan Baxley <ryan@mytrellis.com>
2020-01-09 13:05:15 -0500

    Convert frontend to typescript

---
+++ frontend/src/pages/SensorList/SensorList.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./SensorList.css";
+
+import { getSensors } from "../../services/SensorService";
+
+interface SensorListState {
+  sensors: {}[];
+  loading: boolean;
+  error: string;
+}
+
+class SensorList extends React.Component<{}, SensorListState> {
+  state = {
+    sensors: [],
+    loading: true,
+    error: ""
+  };
+
+  componentDidMount() {
+    getSensors()
+      .then(sensors => this.setState({ sensors, loading: false }))
+      .catch(err => {
+        console.error(err);
+        this.setState({ error: err.message, loading: false });
+      });
+  }
+
+  render() {
+    const { loading, sensors, error } = this.state;
+    if (error) {
+      return <div className="SensorListError">{error}</div>;
+    }
+    if (loading) {
+      return <div className="SensorListLoading">Loading...</div>;
+    }
+    return (
+      <div className="SensorList">
+        {sensors.map(({ id, name, description }) => (
+          <div key={id} className="SensorListItem">
+            <div className="SensorListName">{name}</div>
+            <div className="SensorListDescription">{description}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default SensorList;
